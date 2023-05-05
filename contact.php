32e6db2a9789e6d8f10ff7aa37f76e216725bb99
belloemmanuel <belloemmanuel02@gmail.com>
2023-05-05 14:55:35 +0100

    project type change

---
+++ contact.php
@@ -0,0 +1,326 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<!-- Mirrored from preview.colorlib.com/theme/consulto/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 04 May 2023 18:28:02 GMT -->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>Consulting | Template</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="manifest" href="site.html">
+<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/css/slicknav.css">
+<link rel="stylesheet" href="assets/css/flaticon.css">
+<link rel="stylesheet" href="assets/css/progressbar_barfiller.css">
+<link rel="stylesheet" href="assets/css/gijgo.css">
+<link rel="stylesheet" href="assets/css/animate.min.css">
+<link rel="stylesheet" href="assets/css/animated-headline.css">
+<link rel="stylesheet" href="assets/css/magnific-popup.css">
+<link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+<link rel="stylesheet" href="assets/css/themify-icons.css">
+<link rel="stylesheet" href="assets/css/slick.css">
+<link rel="stylesheet" href="assets/css/nice-select.css">
+<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<div id="preloader-active">
+<div class="preloader d-flex align-items-center justify-content-center">
+<div class="preloader-inner position-relative">
+<div class="preloader-circle"></div>
+<div class="preloader-img pere-text">
+<img src="assets/img/logo/loder.png" alt="">
+</div>
+</div>
+</div>
+</div>
+
+<header>
+
+<div class="header-area header_area">
+<div class="main-header">
+<div class="header-bottom  header-sticky">
+<div class="container-fluid">
+<div class="row align-items-center">
+
+<div class="col-xl-2 col-lg-2 col-md-1">
+<div class="logo">
+<a href="index-2.html"><img src="assets/img/logo/logo.png" alt=""></a>
+</div>
+</div>
+<div class="col-xl-10 col-lg-10 col-md-8">
+<div class="header-left  d-flex f-right align-items-center">
+
+<div class="main-menu f-right d-none d-lg-block">
+<nav>
+<ul id="navigation">
+<li><a href="index-2.html">Home</a></li>
+<li><a href="#">Pages</a>
+<ul class="submenu">
+<li><a href="about.html">about</a></li>
+<li><a href="elements.html">Element</a></li>
+</ul>
+</li>
+<li><a href="services.html">Services</a></li>
+<li><a href="#">Blog</a>
+<ul class="submenu">
+<li><a href="blog.html">Blog</a></li>
+<li><a href="blog_details.html">Blog Details</a></li>
+</ul>
+</li>
+<li><a href="contact.html">Contact</a></li>
+</ul>
+</nav>
+</div>
+
+<div class="header-right-btn f-right d-none d-lg-block  ml-30">
+<a href="#" class="header-btn">Make an Appointment</a>
+</div>
+</div>
+</div>
+
+<div class="col-12">
+<div class="mobile_menu d-block d-lg-none"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</header>
+<main>
+
+<div class="slider-area slider-area2 slider-height2 position-relative" data-background="assets/img/hero/hero2.png">
+<div class="slider-active">
+
+<div class="single-slider">
+<div class="slider-cap-wrapper">
+<div class="hero-caption">
+<h1 data-animation="fadeInLeft" data-delay=".4s">Contact</h1>
+
+<nav aria-label="breadcrumb">
+<ol class="breadcrumb">
+<li class="breadcrumb-item"><a href="index-2.html">Home</a></li>
+<li class="breadcrumb-item"><a href="#">Contact</a></li>
+</ol>
+</nav>
+</div>
+
+<div class="hero-img">
+<img src="assets/img/hero/h1_hero2.png" alt="" data-animation="fadeInRight" data-transition-duration="5s">
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<section class="contact-section">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<h2 class="contact-title">Get in Touch</h2>
+</div>
+<div class="col-lg-8">
+<form class="form-contact contact_form" action="https://preview.colorlib.com/theme/consulto/contact_process.php" method="post" id="contactForm" novalidate="novalidate">
+<div class="row">
+<div class="col-12">
+<div class="form-group">
+<textarea class="form-control w-100" name="message" id="message" cols="30" rows="9" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'" placeholder=" Enter Message"></textarea>
+</div>
+</div>
+<div class="col-sm-6">
+<div class="form-group">
+<input class="form-control valid" name="name" id="name" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" placeholder="Enter your name">
+</div>
+</div>
+<div class="col-sm-6">
+<div class="form-group">
+<input class="form-control valid" name="email" id="email" type="email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" placeholder="Email">
+</div>
+</div>
+<div class="col-12">
+<div class="form-group">
+<input class="form-control" name="subject" id="subject" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Subject'" placeholder="Enter Subject">
+</div>
+</div>
+</div>
+<div class="form-group mt-3">
+<button type="submit" class="button button-contactForm boxed-btn">Send</button>
+</div>
+</form>
+</div>
+<div class="col-lg-3 offset-lg-1">
+<div class="media contact-info">
+<span class="contact-info__icon"><i class="ti-home"></i></span>
+<div class="media-body">
+<h3>Buttonwood, California.</h3>
+<p>Rosemead, CA 91770</p>
+</div>
+</div>
+<div class="media contact-info">
+<span class="contact-info__icon"><i class="ti-tablet"></i></span>
+<div class="media-body">
+<h3>+1 253 565 2365</h3>
+<p>Mon to Fri 9am to 6pm</p>
+</div>
+</div>
+<div class="media contact-info">
+<span class="contact-info__icon"><i class="ti-email"></i></span>
+<div class="media-body">
+<h3><a href="https://preview.colorlib.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="c2b1b7b2b2adb0b682a1adaeadb0aeaba0eca1adaf">[email&#160;protected]</a></h3>
+<p>Send us your query anytime!</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+</main>
+<footer>
+<div class="footer-wrapper section-bg2 pl-100" data-background="assets/img/gallery/footer-bg.png">
+
+<div class="footer-area footer-padding">
+<div class="container-fluid">
+<div class="row">
+<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
+<div class="single-footer-caption mb-50">
+<div class="single-footer-caption mb-30">
+
+<div class="footer-logo mb-35">
+<a href="index-2.html"><img src="assets/img/logo/logo2_footer.png" alt=""></a>
+</div>
+<div class="footer-tittle">
+<div class="footer-pera">
+<p>The automated process starts as soon as your clothes go into the machine.</p>
+</div>
+</div>
+
+<div class="footer-social">
+<a href="#"><i class="fab fa-twitter"></i></a>
+<a href="https://bit.ly/sai4ull"><i class="fab fa-facebook-f"></i></a>
+<a href="#"><i class="fab fa-pinterest-p"></i></a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-2 col-lg-4 col-md-4 col-sm-6">
+<div class="single-footer-caption mb-50">
+<div class="footer-tittle">
+<h4>Our solutions</h4>
+<ul>
+<li><a href="#">Design & creatives</a></li>
+<li><a href="#">Telecommunication</a></li>
+<li><a href="#">Restaurant</a></li>
+<li><a href="#">Programing</a></li>
+<li><a href="#">Architecture</a></li>
+</ul>
+</div>
+</div>
+</div>
+ <div class="col-xl-2 col-lg-4 col-md-4 col-sm-6">
+<div class="single-footer-caption mb-50">
+<div class="footer-tittle">
+<h4>Company</h4>
+<ul>
+<li><a href="#">About</a></li>
+<li><a href="#">Review</a></li>
+<li><a href="#">Insights</a></li>
+<li><a href="#">Programing</a></li>
+<li><a href="#">Carrier</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
+<div class="single-footer-caption mb-50">
+<div class="footer-tittle">
+<h4>Contact us</h4>
+<ul>
+<li><a href="#"><span class="__cf_email__" data-cfemail="88ebe7e6fbfde4fce7b1b0c8efe5e9e1e4a6ebe7e5">[email&#160;protected]</span></a></li>
+<li><a href="#">76/A, Green road, NYC</a></li>
+<li class="number"><a href="#">(80) 783 367-3904</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="footer-bottom-area">
+<div class="container-fluid">
+<div class="footer-border">
+<div class="row align-items-center">
+<div class="col-xl-12 ">
+<div class="footer-copy-right">
+<p>
+Copyright &copy;<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com/" target="_blank">Colorlib</a>
+</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+</footer>
+
+<div id="back-top">
+<a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
+</div>
+
+<script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+
+<script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+<script src="assets/js/jquery.slicknav.min.js"></script>
+
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/slick.min.js"></script>
+
+<script src="assets/js/wow.min.js"></script>
+<script src="assets/js/animated.headline.js"></script>
+<script src="assets/js/jquery.magnific-popup.js"></script>
+
+<script src="assets/js/gijgo.min.js"></script>
+
+<script src="assets/js/jquery.nice-select.min.js"></script>
+<script src="assets/js/jquery.sticky.js"></script>
+
+<script src="assets/js/jquery.counterup.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<script src="assets/js/jquery.countdown.min.js"></script>
+<script src="assets/js/hover-direction-snake.min.js"></script>
+
+<script src="assets/js/contact.js"></script>
+<script src="assets/js/jquery.form.js"></script>
+<script src="assets/js/jquery.validate.min.js"></script>
+<script src="assets/js/mail-script.js"></script>
+<script src="assets/js/jquery.ajaxchimp.min.js"></script>
+
+<script src="assets/js/plugins.js"></script>
+<script src="assets/js/main.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v52afc6f149f6479b8c77fa569edb01181681764108816" integrity="sha512-jGCTpDpBAYDGNYR5ztKt4BQPGef1P0giN6ZGVUi835kFF88FOmmn8jBQWNgrNd8g/Yu421NdgWhwQoaOPFflDw==" data-cf-beacon='{"rayId":"7c22cbbd49e51c86","version":"2023.4.0","b":1,"token":"cd0b4b3a733644fc843ef0b185f98241","si":100}' crossorigin="anonymous"></script>
+</body>
+
+<!-- Mirrored from preview.colorlib.com/theme/consulto/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 04 May 2023 18:28:03 GMT -->
+</html>
